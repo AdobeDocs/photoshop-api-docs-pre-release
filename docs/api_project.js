d502aeef38e2cb7f17180976ac143d90d526f1d4
khound <khound@adobe.com>
2020-04-21 16:31:01 -0700

    API docs for blendMode

--- docs/api_project.js
+++ docs/api_project.js
@@ -1,15 +1,1 @@
-define({
-  "name": "Photoshop API Docs",
-  "version": "0.1.0",
-  "description": "Your interface to Adobe Photoshop and Imaging API's",
-  "title": "Photoshop API Docs",
-  "sampleUrl": false,
-  "defaultVersion": "0.0.0",
-  "apidoc": "0.3.0",
-  "generator": {
-    "name": "apidoc",
-    "time": "2020-04-20T22:16:53.103Z",
-    "url": "http://apidocjs.com",
-    "version": "0.20.1"
-  }
-});
+define({  "name": "Photoshop API Docs",  "version": "0.1.0",  "description": "Your interface to Adobe Photoshop and Imaging API's",  "title": "Photoshop API Docs",  "sampleUrl": false,  "defaultVersion": "0.0.0",  "apidoc": "0.3.0",  "generator": {    "name": "apidoc",    "time": "2020-04-20T22:47:37.520Z",    "url": "http://apidocjs.com",    "version": "0.17.7"  }});
