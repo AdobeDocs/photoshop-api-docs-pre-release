0fcb32b8df4a8142290510a5edb894b513544c41
linhu <linhu@adobe.com>
2020-07-21 16:17:02 -0700

    add IO support

--- docs/api_project.js
+++ docs/api_project.js
@@ -8,8 +8,8 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2020-06-18T20:15:52.926Z",
+    "time": "2020-07-21T18:56:57.226Z",
     "url": "http://apidocjs.com",
-    "version": "0.22.1"
+    "version": "0.24.0"
   }
 });
