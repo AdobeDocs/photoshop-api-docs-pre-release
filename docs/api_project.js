aa661b06f2bfb0c447933da4a1c906fcb221914c
khound <khound@adobe.com>
2021-03-25 17:08:32 -0700

    bringing in API doc changes from https://git.corp.adobe.com/dice/Creative-Imaging-Services-API-docs/pull/26

--- docs/api_project.js
+++ docs/api_project.js
@@ -8,7 +8,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2021-02-11T20:24:01.878Z",
+    "time": "2021-03-25T23:18:22.308Z",
     "url": "https://apidocjs.com",
     "version": "0.26.0"
   }
